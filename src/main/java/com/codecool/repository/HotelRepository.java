793702bec38dfba268e659b03ec128b58ee2ea57
Paul <bielcu@gmail.com>
2019-03-06 13:42:42 +0100

    Comment not working part of code

--- src/main/java/com/codecool/repository/HotelRepository.java
+++ src/main/java/com/codecool/repository/HotelRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface HotelRepository extends CrudRepository<Hotel, Integer> {
 
-    List<Hotel> findByStars(Integer stars);
-    List<Hotel> findByCity(String city);
+//    List<Hotel> findByStars(Integer stars);
+//    List<Hotel> findByCity(String city);
     //Hotel removeHotelByName(String hotel_name);
 }
