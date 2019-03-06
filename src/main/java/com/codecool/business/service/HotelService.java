0096b0402b8c4c544e6671d7d23b643fddff7862
SStreich <selene.streich@gmail.com>
2019-03-06 12:19:05 +0100

    Create HotelService class with constructor and hotels searching by name method

--- src/main/java/com/codecool/business/service/HotelService.java
+++ src/main/java/com/codecool/business/service/HotelService.java
@@ -1,8 +1,15 @@
-package com.codecool.service;
+package com.codecool.business.service;
 
+import com.codecool.business.domain.HotelResearch;
+import com.codecool.entity.Hotel;
 import com.codecool.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class HotelService {
@@ -11,5 +18,17 @@ public class HotelService {
     @Autowired
     public HotelService(HotelRepository hotelRepository) {
         this.hotelRepository = hotelRepository;
+    }
+
+    public List<Hotel> getHotelsByName(String hotelName) {
+        Iterable<Hotel> hotels = this.hotelRepository.findAll();
+        List<Hotel> hotelList = new ArrayList<>();
+
+        hotels.forEach(hotel->{
+            if(hotel.getName().contains(hotelName)) {
+                hotelList.add(hotel);
+            }
+        });
+        return hotelList;
     }
 }
