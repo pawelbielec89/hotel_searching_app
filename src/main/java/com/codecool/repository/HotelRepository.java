968173e4a7657a7895eb8af328a325009fe79343
SStreich <selene.streich@gmail.com>
2019-03-06 12:16:24 +0100

    Create repository for Hotel class

--- src/main/java/com/codecool/repository/HotelRepository.java
+++ src/main/java/com/codecool/repository/HotelRepository.java
@@ -1,4 +1,15 @@
 package com.codecool.repository;
 
-public class HotelRepository {
+import com.codecool.entity.Hotel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HotelRepository extends CrudRepository<Hotel, Integer> {
+
+    List<Hotel> findByStars(Integer stars);
+    List<Hotel> findByCity(String city);
+    //Hotel removeHotelByName(String hotel_name);
 }
