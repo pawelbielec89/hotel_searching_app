b773cc647c7a0ef10cb403a877314c796b1b1bd6
SStreich <selene.streich@gmail.com>
2019-03-06 12:18:00 +0100

    Create HotelResearch domain class with getters and setters

--- src/main/java/com/codecool/business/domain/HotelResearch.java
+++ src/main/java/com/codecool/business/domain/HotelResearch.java
@@ -1,4 +1,60 @@
 package com.codecool.business.domain;
 
 public class HotelResearch {
+    private int hotelId;
+    private String hotelName;
+    private String adress;
+    private String website;
+    private String city;
+    private int rate;
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
 }
