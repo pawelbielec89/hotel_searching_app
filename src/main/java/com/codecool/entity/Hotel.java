29d2eb5b00da561c8c0f2995feea0bb538f5a9fd
SStreich <selene.streich@gmail.com>
2019-03-08 02:14:57 +0100

    Fix primary key hotel.id to serial primary key Add new column is_active Add getter and setter to is_active field in Hotel

--- src/main/java/com/codecool/entity/Hotel.java
+++ src/main/java/com/codecool/entity/Hotel.java
@@ -1,6 +1,8 @@
 package com.codecool.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.*;
@@ -8,10 +10,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@org.hibernate.annotations.DynamicInsert
 @Table(name = "hotel")
 public class Hotel {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Generated(GenerationTime.INSERT)
+
+    @Column(columnDefinition = "serial")
     private Integer id;
     @Column(name = "hotel_name")
     private String name;
@@ -21,38 +27,52 @@ public class Hotel {
     private String adress;
     @Column(name = "email")
     private String email;
+    @Column(name = "rate")
+    private Integer rate;
     @Column(name = "website")
     private String website;
-    @Column(name = "rate")
-    private int rate;
+
+    public Boolean getIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(Boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    @Column(name = "is_active", columnDefinition = "boolean default true")
+    private Boolean is_active;
 
     @OneToMany(mappedBy = "hotel",
                 cascade = CascadeType.ALL,
                 orphanRemoval = true)
     private List<Room> rooms = new ArrayList<>();
 
-//    public Hotel(@JsonProperty("hotel_name") String name, @JsonProperty("city") String city,
-//          @JsonProperty("adress") String adress, @JsonProperty("email") String email,
-//          @JsonProperty("website") String website) {
-//        this.name = name;
-//        this.city = city;
-//        this.adress = adress;
-//        this.website = website;
-//        this.email = email;
-//    }
-
-    public Hotel() {}
-
     @Autowired(required = false)
-    public Hotel(@JsonProperty("hotel_name") String name) {
+    public Hotel(@JsonProperty("hotel_name") String name, @JsonProperty("city") String city,
+          @JsonProperty("adress") String adress, @JsonProperty("email") String email,
+          @JsonProperty("website") String website, @JsonProperty("rate") int rate) {
         this.name = name;
+        this.city = city;
+        this.adress = adress;
+        this.email = email;
+        this.website = website;
+        this.rate = rate;
+
     }
 
-    public int getId() {
+    public Hotel() {}
+//
+//    @Autowired(required = false)
+//    public Hotel(@JsonProperty("hotel_name") String name) {
+//        this.name = name;
+//    }
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -96,11 +116,11 @@ public class Hotel {
         this.website = website;
     }
 
-    public int getRate() {
+    public Integer getRate() {
         return rate;
     }
 
-    public void setRate(int rate) {
+    public void setRate(Integer rate) {
         this.rate = rate;
     }
 
