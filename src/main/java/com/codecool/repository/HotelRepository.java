b24b004e8b89e411df53ec12743df8337a7f07f9
SStreich <selene.streich@gmail.com>
2019-03-08 02:18:06 +0100

    Remove never implemented methods from HotelRepository interface

--- src/main/java/com/codecool/repository/HotelRepository.java
+++ src/main/java/com/codecool/repository/HotelRepository.java
@@ -4,20 +4,12 @@ import com.codecool.entity.Hotel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
 public interface HotelRepository extends CrudRepository<Hotel, Integer> {
 
- List<Hotel> findByRate(Integer rate);
-
- List<Hotel> findByCity(String city);
-
  default Hotel findHotelByName(String hotel_name) {
-
   for (Hotel hotel : this.findAll()) {
-   if (hotel.getName().contains(hotel_name)) {
+   if (hotel.getName().toLowerCase().equals(hotel_name.toLowerCase().replace('_', ' '))) {
     return hotel;
    }
   }
