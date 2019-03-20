02459f9d228b925880346f738e0a98528c65414c
SStreich <selene.streich@gmail.com>
2019-03-20 12:13:22 +0100

    Refactor - change path name in HotelController to /hotels-view/

--- src/main/java/com/codecool/web/application/HotelController.java
+++ src/main/java/com/codecool/web/application/HotelController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/hotels")
+@RequestMapping(value = "/hotels-view")
 public class HotelController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String getHotels(){
-        return "hotels";
+        return "hotels-view";
     }
 }
