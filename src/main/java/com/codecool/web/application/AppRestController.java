6658c020640ce6cd04c4524c22b8b0a8c967e545
SStreich <selene.streich@gmail.com>
2019-03-20 12:09:54 +0100

    Refactor - change path name in AppRestController to /hotels/

--- src/main/java/com/codecool/web/application/AppRestController.java
+++ src/main/java/com/codecool/web/application/AppRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping(value = "hotels2")
+@RequestMapping(value = "hotels")
 @RestController
 public class AppRestController {
 
